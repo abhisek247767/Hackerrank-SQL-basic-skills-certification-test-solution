6d40b53d5095a8c9d80b5127dbf46457736d7ac3
Abhisek Roy <69287212+abhisek247767@users.noreply.github.com>
2022-07-08 22:32:41 +0530

    Update student-advisor.sql

--- student-advisor.sql
+++ student-advisor.sql
@@ -2,4 +2,4 @@ SELECT roll_number,name
 FROM student_information s
 INNER JOIN faculty_information f
 ON s.advisor = f.employee_ID
-WHERE (s.gender = 'M' and f.salary>15000) or (s.gender = 'F' and f.salary>20000);
+WHERE (f.gender = 'M' and f.salary>15000) or (f.gender = 'F' and f.salary>20000);
